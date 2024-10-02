1542a2ba4ccdfcf4c2751be43a8fbe2ed45487b0
maplesoup1 <timmy000728@gmail.com>
2024-10-02 20:18:59 +1000

    quick fix

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
+interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
@@ -17,4 +19,5 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttribu
 );
 
 Textarea.displayName = "Textarea";
+
 export { Textarea };
